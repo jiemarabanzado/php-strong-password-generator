b608689728017393fd52def1e121c9321dfd9d1f
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-12-16 01:11:45 +0100

    checkpoint

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
     $letters=array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-    
+    $numbers=array(0,1,2,3,4,5,6,7,8,9);
     $upperCase=array();
     for($i=0;$i<count($letters);$i++){
         $temp=strtoupper( $letters[$i]);
@@ -8,16 +8,8 @@
     }
 
     $passLength=$_GET["length"];
-
-    function Test(){
-        if(empty($passLength)){
-             echo 'inserisci una lunghezza per la password';
-        }else{
-            echo $passLength;
-            var_dump($passLength);
-        }
-    }
-
+    $ripetition=$_GET["ripetition"];
+    
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +27,83 @@
         <h1 class="pt-4">Strong Password Generator</h1>
         <h3 class="mb-4">Genera una password sicura</h3>
         <div class="ms-container mb-3">
-            <?php Test() ?>
+            <?php if(empty($passLength)){
+                    echo 'inserisci una lunghezza per la password';
+                    }else{
+                        if($ripetition=='no-repeat'){
+                            $lowercaseUsed=array();
+                            $uppercaseUsed=array();
+                            $numberUsed=array();
+                            for ($i=0; $i < $passLength; $i++) { 
+                                $isFull=true;
+                                    while($isFull){
+                                        if(count($lowercaseUsed)==count($letters)||count($uppercaseUsed)==count($upperCase)||count($numberUsed)==count($numbers)){
+                                            $isFull=true;
+                                        }else{
+                                            $char=rand(1,3);
+                                            if($char==1){
+                                                $isInLower=true;
+                                                while($isInLower){
+                                                    $randomIndex=rand(0,count($letters)-1);
+                                                    if(in_array($letters[$randomIndex],$lowercaseUsed)){
+                                                        $isInLower= true;
+                                                    }else{
+                                                        echo $letters[$randomIndex] ;
+                                                        array_push($lowercaseUsed,$letters[$randomIndex]);
+                                                        $isInLower=false;
+                                                    }
+                                                }
+                                            }else{
+                                                if($char==2){
+                                                $isInUpper=true;
+                                                while($isInUpper){
+                                                    $randomIndex=rand(0,count($letters)-1);
+                                                    if(in_array($upperCase[$randomIndex],$uppercaseUsed)){
+                                                        $isInUpper= true;
+                                                    }else{
+                                                        echo $upperCase[$randomIndex] ;
+                                                        array_push($uppercaseUsed,$upperCase[$randomIndex]);
+                                                        $isInUpper=false;
+                                                    }
+                                                }
+                                                }else{
+                                                    $isInNumbers=true;
+                                                    while($isInNumbers){
+                                                        $randomIndex=rand(0,9);
+                                                        if(in_array($randomIndex,$numberUsed)){
+                                                            $isInNumbers= true;
+                                                        }else{
+                                                            echo $randomIndex ;
+                                                            array_push($numberUsed,$randomIndex);
+                                                            $isInNumbers=false;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            $isFull=false;
+                                        }
+                                    } 
+                            }           
+                        }else{
+                            for ($i=0; $i < $passLength; $i++) { 
+                                $char=rand(1,3);
+                                if($char==1){                                  
+                                    $randomIndex=rand(0,count($letters)-1);                                      
+                                    echo $letters[$randomIndex] ;    
+                                }
+                                else{
+                                    if($char==2){
+                                        $randomIndex=rand(0,count($letters)-1);
+                                        echo $upperCase[$randomIndex] ;
+                                    }else{
+                                        $randomIndex=rand(0,9);
+                                        echo $randomIndex;
+                                    }
+                                }
+                            }
+                        } 
+                    }            
+                ?>
         </div>
         <div class="form ms-container text-start">
             <div>
@@ -45,7 +113,7 @@
                             <div>lunghezza password:</div>
                         </div>
                         <div class="col-5">
-                            <input type="number" required name="length">
+                            <input type="number" required name="length" min="4" max="40">
                         </div>
                     </div>
                     <div class="ripetition row">
@@ -59,7 +127,7 @@
                                     
                                 </div>
                                 <div>
-                                <input type="checkbox" name="ripetition" value="no-repeat"> Non consentire
+                                <input type="checkbox" name="ripetition" value="no-repeat" checked> Non consentire
                                     
                                 </div>
                             </div>
